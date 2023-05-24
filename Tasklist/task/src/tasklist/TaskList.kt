f09b0a66c4d761e2fd0bb237988f2cdaa99853b5
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-05-24 17:35:37 +0100

    added more input validation and two more options

--- Tasklist/task/src/tasklist/TaskList.kt
+++ Tasklist/task/src/tasklist/TaskList.kt
@@ -1,25 +1,47 @@
 package tasklist
 
 object TaskList {
-    private val tasks = mutableListOf<String>()
+    private val tasks: MutableList<MutableList<String>> = mutableListOf()
 
     fun start() {
-        println("Input the tasks (enter a blank line to end):")
-        readTasks()
-        printTasks()
+
+        while (true) {
+            println("Input an action (add, print, end):")
+            when (readln().trim().lowercase()) {
+                "add" -> addTasks()
+                "print" -> printTasks()
+                "end" -> break
+                else -> println("The input action is invalid")
+            }
+        }
+        println("Tasklist exiting!")
     }
 
-    private fun readTasks() {
+    private fun addTasks() {
+        println("Input a new task (enter a blank line to end):")
+
+        val inputTasks = mutableListOf<String>()
         while (true) {
-            val task = readln()
+            val task = readln().trim()
             if (task.isBlank()) break
-            tasks.add(task)
+            inputTasks.add(task)
         }
+
+        if (inputTasks.isEmpty()) println("The task is blank")
+        else tasks.add(inputTasks)
     }
 
     private fun printTasks() {
+        if (tasks.isEmpty()) {
+            println("No tasks have been input")
+            return
+        }
         for (i in tasks.indices) {
-            println(String.format("%-2d ${tasks[i]}", (i+1)))
+            println(String.format("%-2d ${tasks[i].first()}", (i+1)))
+            for (item in tasks[i].subList(1, tasks[i].size)) {
+                println("   $item")
+            }
+            println()
         }
     }
 }
