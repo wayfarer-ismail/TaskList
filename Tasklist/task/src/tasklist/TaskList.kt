149c0a9f2a415cbda85a1be3ec8a5d251f8b4379
wayfarer-ismail <ismailelbahja03@gmail.com>
2023-05-24 11:17:35 +0100

    reads and prints inputted tasks

--- Tasklist/task/src/tasklist/TaskList.kt
+++ Tasklist/task/src/tasklist/TaskList.kt
@@ -1,7 +1,25 @@
 package tasklist
 
 object TaskList {
+    private val tasks = mutableListOf<String>()
+
     fun start() {
         println("Input the tasks (enter a blank line to end):")
+        readTasks()
+        printTasks()
+    }
+
+    private fun readTasks() {
+        while (true) {
+            val task = readln()
+            if (task.isBlank()) break
+            tasks.add(task)
+        }
+    }
+
+    private fun printTasks() {
+        for (i in tasks.indices) {
+            println(String.format("%-2d ${tasks[i]}", (i+1)))
+        }
     }
 }
